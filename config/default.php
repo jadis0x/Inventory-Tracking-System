e8df3a098104c28acad10c561c137f7b247d3d13
Jadis <49281043+jadis0x@users.noreply.github.com>
2022-06-01 20:07:46 +0300

    Add files via upload

---
+++ config/default.php
@@ -0,0 +1,18 @@
+<?php
+
+define("CONTROLLERS_PATH", "Application/controllers");
+define("VIEWS_PATH", "Application/views");
+define("MODELS_PATH", "Application/models");
+define("DB_HOST", "localhost");
+define("DB_NAME", "sto");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("SITE_URL", "http://localhost");
+
+define("CSS_PATH", SITE_URL . "/public/css");
+define("JS_PATH", SITE_URL . "/public/js");
+define("IMG_PATH", SITE_URL . "/public/img");
+define("BOWER_PATH", SITE_URL . "/public/bower_components");
+define("PLUGIN_PATH", SITE_URL . "/public/plugins");
+
+define("IZINLER", ["Kategori", "Ürünler", "Müşteriler", "Stok", "Kasa", "Faturalar", "Kullanıcılar", "Raporlar"]);
---
+++ config/sessionManager.php
@@ -0,0 +1,52 @@
+<?php
+
+class sessionManager extends model
+{
+    static function createSession($array = [])
+    {
+        foreach ($array as $key => $value) {
+            $_SESSION[$key] = helper::cleaner($value);
+        }
+    }
+
+    static function deleteSession($key)
+    {
+        unset($_SESSION[$key]);
+    }
+
+    static function allSessionDelete()
+    {
+        session_destroy();
+    }
+
+    public function isLogged()
+    {
+        if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
+            $email = $_SESSION['email'];
+            $password = $_SESSION['password'];
+
+            $query = $this->db->prepare("SELECT * FROM uyeler WHERE email = ? AND sifre = ?");
+            $query->execute(array($email, $password));
+
+            if ($query->rowCount() != 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public function getUserInfo()
+    {
+        if ($this->isLogged()) {
+            $sorgu = $this->db->prepare("SELECT * FROM uyeler WHERE email = ? and sifre = ?");
+            $sorgu->execute(array($_SESSION['email'], $_SESSION['password']));
+
+            return $sorgu->fetch(PDO::FETCH_ASSOC);
+        } else {
+            return false;
+        }
+    }
+}
